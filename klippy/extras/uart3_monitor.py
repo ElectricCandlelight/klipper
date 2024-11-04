0760c49f4273a85ef4c768de82da992231e9b5cf
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-04 02:13:01 +0000

    Message buffer

--- klippy/extras/uart3_monitor.py
+++ klippy/extras/uart3_monitor.py
@@ -1,15 +1,27 @@
+# File: klippy/extras/uart3_monitor.py
+
 class Uart3Monitor:
     def __init__(self, config):
         self.printer = config.get_printer()
-        # Register handler for uart3_rx messages
         self.mcu = self.printer.lookup_object('mcu')
         self.mcu.register_response(self._handle_uart3_rx, "uart3_rx byte=%c")
+        self.buffer = []
         
     def _handle_uart3_rx(self, params):
-        # Get byte from response
-        byte = params['byte'].decode('utf-8')
-        # Log to console
-        self.printer.get_reactor().process_message("uart3_rx: " + byte)
+        byte = params['byte']
+        
+        # Convert numeric byte value to character
+        if byte == 13:  # CR
+            return
+        if byte == 10:  # LF
+            # Process complete message
+            message = ''.join([chr(x) for x in self.buffer])
+            self.printer.get_reactor().process_message("uart3_rx: " + message)
+            self.buffer = []
+            return
+            
+        # Add byte to buffer
+        self.buffer.append(byte)
 
 def load_config(config):
     return Uart3Monitor(config)
