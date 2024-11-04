a89efdabea676e1bb013db19a948e72dd9ef771f
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-04 02:59:44 +0000

    fix logging

--- klippy/extras/uart3_monitor.py
+++ klippy/extras/uart3_monitor.py
@@ -11,20 +11,15 @@ class Uart3Monitor:
         self.mcu = self.printer.lookup_object('mcu')
         self.mcu.register_response(self._handle_uart3_rx, "uart3_rx msg=%s")
         # Get base logger used by Klipper
-        self.logger = logging.getLogger('klippy')
         print("UART3Monitor initialized") # Debug print
+        logging.info("Starting UART3 monitor") # Log to klippy.log
         
     def _handle_uart3_rx(self, params):
-        print("Handle UART3 called with:", params) # Debug print
-        try:
+            logging.info("new message received") # Debug print
             message = params['msg'].decode('utf-8').strip()
             # Log to klippy.log
-            self.logger.info("UART3: %s", message)
+            logging.info("UART3: %s", message)
             # Display in Mainsail console using reactor
-            self.printer.get_reactor().process_message("UART3: " + message)
-        except Exception as e:
-            print("Error in handle_uart3:", str(e)) # Debug print
-            self.logger.error("UART3 error: %s", str(e))
 
 def load_config(config):
     return Uart3Monitor(config)
