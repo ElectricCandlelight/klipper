cededf941121418b18aff40ec0cb07fa76823e06
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-04 02:42:40 +0000

    Fix logging

--- klippy/extras/uart3_monitor.py
+++ klippy/extras/uart3_monitor.py
@@ -1,20 +1,26 @@
-# File: klippy/extras/uart3_monitor.py
+# UART3 monitoring for Klipper
+#
+# Copyright (C) 2024 <Your Name>
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+import logging
 
 class Uart3Monitor:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.mcu = self.printer.lookup_object('mcu')
+        # Register response handler for uart3_rx messages
         self.mcu.register_response(self._handle_uart3_rx, "uart3_rx msg=%s")
-        # Get correct logger instance
-        self.logger = config.get_printer().get_start_args()['log_file']
+        # Set up logging
+        self.gcode = self.printer.lookup_object('gcode')
         
     def _handle_uart3_rx(self, params):
         # Convert bytes to string and strip CR/LF
-        message = params['msg'].decode('utf-8').strip()
-        # Log to klippy.log
-        print("UART3:", message, file=self.logger)
-        # Optional console display
-        self.printer.get_reactor().process_message("uart3_rx: " + message)
+        message = params['msg'].strip()
+        # Log using Klipper's logging system
+        logging.info("UART3: %s", message)
+        # Also display in console
+        self.gcode.respond_info("UART3: %s" % message)
 
 def load_config(config):
     return Uart3Monitor(config)
