db04d9a62c3872916e13b769f1b51612c8125050
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-04 02:36:13 +0000

    Fix logging

--- klippy/extras/uart3_monitor.py
+++ klippy/extras/uart3_monitor.py
@@ -4,14 +4,16 @@ class Uart3Monitor:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.mcu = self.printer.lookup_object('mcu')
-        # Register for complete messages instead of bytes
         self.mcu.register_response(self._handle_uart3_rx, "uart3_rx msg=%s")
+        self.logger = self.printer.get_logger()
         
     def _handle_uart3_rx(self, params):
-        # Get complete message
-        message = params['msg']
+        # Convert bytes to string and strip CR/LF
+        message = params['msg'].decode('utf-8').strip()
         # Display message
-        self.printer.get_reactor().process_message("uart3_rx: " + message)
+        #self.printer.get_reactor().process_message("uart3_rx: " + message)
+        self.logger.info("New message from UART3")
+        self.logger.info("code: %s", message)
 
 def load_config(config):
     return Uart3Monitor(config)
