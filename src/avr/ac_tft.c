f90323cddb16fe2066f792b80bfffec7baeea780
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-12 01:28:11 +0000

    fix typo

--- src/avr/ac_tft.c
+++ src/avr/ac_tft.c
@@ -11,7 +11,7 @@ void uart3_init(void)
     uint32_t cm = 16;
     uint32_t div = DIV_ROUND_CLOSEST(CONFIG_CLOCK_FREQ, cm * UART3_BAUD) - 1UL;
     UBRR3H = div >> 8;
-    UBR3L = div;
+    UBRR3L = div;
     UCSR3B = (1 << RXEN3) | (1 << TXEN3) | (1 << RXCIE3);
     UCSR3C = (1 << UCSZ31) | (1 << UCSZ30);
     sendf("UART3 initialized with baud rate %u", UART3_BAUD);
