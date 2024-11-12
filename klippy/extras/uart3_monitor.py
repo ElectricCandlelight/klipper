c526146171cd864ba7a77ca82cfaed02fb22430f
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-12 00:41:04 +0000

    re name uart

--- klippy/extras/uart3_monitor.py
+++ klippy/extras/uart3_monitor.py
@@ -1,10 +1,4 @@
-# UART3 monitoring for Klipper
-#
-# Copyright (C) 2024 <Your Name>
-# This file may be distributed under the terms of the GNU GPLv3 license.
-
 import logging
-
 
 class Uart3Monitor:
     def __init__(self, config):
@@ -45,46 +39,6 @@ class Uart3Monitor:
                 logging.warning("Print time")
             case "A8":
                 logging.warning("Print list")
-            case "A9":
-                logging.warning("Pause print")
-            case "A10":
-                logging.warning("Resume print")
-            case "A11":
-                logging.warning("Cancel print")
-            case "A12":
-                logging.warning("Emergency stop")
-            case "A13":
-                logging.warning("Select file")
-            case "A14":
-                logging.warning("Reprint")
-            case "A15":
-                logging.warning("Breakpoint resume")
-            case "A16":
-                logging.warning("Set extruder temperature")
-            case "A17":
-                logging.warning("Set bed temperature")
-            case "A18":
-                logging.warning("Set fan speed")
-            case "A19":
-                logging.warning("Turn off motors")
-            case "A20":
-                logging.warning("Set print speed")
-            case "A21":
-                logging.warning("Reset")
-            case "A22":
-                logging.warning("Move axis")
-            case "A23":
-                logging.warning("Preheat PLA")
-            case "A24":
-                logging.warning("Preheat ABS")
-            case "A25":
-                logging.warning("Cooling")
-            case "A26":
-                logging.warning("Refresh print list")
-            case _:
-                logging.warning("Unknown command: %s", message)
-        # Display in Mainsail console correctly
-
 
 def load_config(config):
     return Uart3Monitor(config)
