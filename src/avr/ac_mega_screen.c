a012968e8cc158f8cd6259813559ab8e6ae765c4
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-23 03:23:41 +0000

    Add in listen for uart

--- src/avr/ac_mega_screen.c
+++ src/avr/ac_mega_screen.c
@@ -11,9 +11,35 @@ void uart3_init(void)
 {
     DDRB |= (1 << PB7); 
     PORTB &= ~(1 << PB7);
+    uint32_t cm = 16;
+    uint32_t div = DIV_ROUND_CLOSEST(CONFIG_CLOCK_FREQ, cm * UART3_BAUD) - 1UL;
+    UBRR3H = div >> 8;
+    UBRR3L = div;
+    // Enable transmitter and receiver
+    UCSR3B = (1 << RXEN3) | (1 << TXEN3) | (1 << RXCIE3);
+    // Set 8N1 frame format
+    UCSR3C = (1 << UCSZ31) | (1 << UCSZ30);
 }
 
 DECL_COMMAND(uart3_init, "config_uart3 oid=%c");
+
+static char uart_buf[UART_BUF_SIZE];
+static uint8_t buf_pos;
+
+ISR(USART3_RX_vect) 
+{
+    uint8_t data = UDR3;
+    
+    if (buf_pos < UART_BUF_SIZE-1) {
+        uart_buf[buf_pos++] = data;
+    }
+    
+    if (data == '\n' || buf_pos >= UART_BUF_SIZE-1) {
+        uart_buf[buf_pos] = '\0';
+        sendf("uart3_rx msg=%s", uart_buf);
+        buf_pos = 0;
+    }
+}
 
 void uart3_send(uint8_t *args)
 {
