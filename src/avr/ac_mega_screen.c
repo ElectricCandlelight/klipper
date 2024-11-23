0a3d478d2944affef0e6784118dfc1c2535f959d
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-23 04:59:16 +0000

    Fix =

--- src/avr/ac_mega_screen.c
+++ src/avr/ac_mega_screen.c
@@ -59,7 +59,7 @@ void command_uart3_tx(uint8_t *args)
 {
     PORTB ^= (1 << PB7);
     // Get the message to transmit
-    char *message = = command_decode_ptr(args[1]);
+    char *message = command_decode_ptr(args[1]);
 
     // Transmit the message
     while (*message) {
