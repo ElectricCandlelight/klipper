247a30a074c9278b1161ee0fe0810d523a39c01d
ElectricCandlelight <ecandlelight@gmail.com>
2024-11-23 01:04:55 +0000

    added pin

--- src/avr/ac_mega_screen.c
+++ src/avr/ac_mega_screen.c
@@ -2,6 +2,7 @@
 #include "command.h"  // sendf
 #include "sched.h"    // DECL_INIT
 #include "autoconf.h" // CONFIG_CLOCK_FREQ
+#include <avr/io.h>
 
 #define UART3_BAUD 115200
 #define UART_BUF_SIZE 64
